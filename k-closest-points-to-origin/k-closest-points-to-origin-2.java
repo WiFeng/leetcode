2c497c4e9ef6b9b7c4384a738ede6f8effcfe307
Kevon Liu <admin@521-wf.com>
2020-07-31 11:10:14 +0800

    repeat k-closest-points-to-origin-2.java

--- k-closest-points-to-origin/k-closest-points-to-origin-2.java
+++ k-closest-points-to-origin/k-closest-points-to-origin-2.java
@@ -1,6 +1,6 @@
 class Solution {
 
-    int [][] points;
+    int[][] points;
 
     public int[][] kClosest(int[][] points, int K) {
         this.points = points;
@@ -9,29 +9,32 @@ class Solution {
     }
 
     private void work(int i, int j, int K) {
-        
-        if (i >= j) {
-            return;
-        }
+        if (i >= j) return;
 
         int oi = i, oj = j;
         int pivotIndex = ThreadLocalRandom.current().nextInt(i, j);
         int pivotVal = dist(pivotIndex);
-    
+
         while (i < j) {
             while (i < j && dist(i) < pivotVal) i++;
             while (i < j && dist(j) > pivotVal) j--;
-            swap(i, j);
+            
             if (dist(i) == pivotVal && dist(j) == pivotVal) {
                 break;
             }
+
+            swap(i, j);
         }
 
         if (i - oi + 1 >= K) {
-            work(oi, i, K);
+            work(oi, j, K);
         } else {
-            work(i + 1, oj, K - (i - oi + 1));
+            work(i+1, oj, K - (i - oi + 1));
         }
+    }
+
+    private int dist(int i) {
+        return points[i][0] * points[i][0] + points[i][1] * points[i][1];
     }
 
     private void swap(int i, int j) {
@@ -42,8 +45,5 @@ class Solution {
         points[j][1] = t1;
     }
 
-    private int dist(int i) {
-        return points[i][0] * points[i][0] + points[i][1] * points[i][1];
-    }
     
 }
