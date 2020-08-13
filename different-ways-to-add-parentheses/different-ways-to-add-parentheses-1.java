0932515ed4361ffde1b165c798d9e4ae9dee9510
Kevon Liu <admin@521-wf.com>
2020-08-13 14:57:37 +0800

    add different-ways-to-add-parentheses.java

---
+++ different-ways-to-add-parentheses/different-ways-to-add-parentheses-1.java
@@ -0,0 +1,56 @@
+class Solution {
+    public List<Integer> diffWaysToCompute(String input) {
+        if (input.length() == 0) {
+            return new ArrayList<>();
+        }
+
+        List<Integer> result = new ArrayList<>();
+
+        int nums = 0;
+        int index;
+        for (index = 0; index < input.length() && !isOperator(input.charAt(index)); index++) {
+            nums = 10 * nums + (input.charAt(index) - '0');
+        }
+
+        if (index == input.length()) {
+            result.add(nums);
+            return result;
+        }
+
+        for (index = 0; index < input.length(); index++) {
+            if (isOperator(input.charAt(index))) {
+                char op = input.charAt(index);
+                List<Integer> result1 = diffWaysToCompute(input.substring(0, index));
+                List<Integer> result2 = diffWaysToCompute(input.substring(index + 1, input.length()));
+                for (int i = 0; i < result1.size(); i++) {
+                    for (int j = 0; j < result2.size(); j++) {
+                        int temp = caculate(result1.get(i), op, result2.get(j));
+                        result.add(temp);
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private int caculate(int num1, char op, int num2) {
+        int result = 0;
+        if (op == '-') {
+            result = num1 - num2;
+        } else if (op == '+') {
+            result = num1 + num2;
+        } else if (op == '*') {
+            result = num1 * num2;
+        }
+
+        return result;
+    }
+
+    private boolean isOperator(char c) {
+        if (c == '-' || c == '+' || c == '*') {
+            return true;
+        }
+        return false;
+    }
+}
