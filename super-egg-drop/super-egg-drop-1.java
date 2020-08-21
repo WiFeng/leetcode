f42b0dc7946a6a679e47818e6b5a0733bf931bb2
Kevon Liu <admin@521-wf.com>
2020-08-21 09:28:09 +0800

    repeat super-egg-drop-1.java

--- super-egg-drop/super-egg-drop-1.java
+++ super-egg-drop/super-egg-drop-1.java
@@ -1,7 +1,7 @@
 class Solution {
 
     private Map<Integer, Integer> memo = new HashMap<>();
-    
+
     public int superEggDrop(int K, int N) {
         return dp(K, N);
     }
@@ -12,7 +12,7 @@ class Solution {
             return memo.get(key);
         }
 
-        int ans;
+        int ans = 0;
         if (N == 0) {
             ans = 0;
         } else if (K == 1) {
@@ -20,25 +20,28 @@ class Solution {
         } else {
             int lo = 1, hi = N;
             while (lo + 1 < hi) {
-                int x = (lo + hi) / 2;
-                int t1 = dp(K-1, x-1);
-                int t2 = dp(K, N-x);
-
+                int m = (lo + hi) / 2;
+                int t1 = dp(K-1, m-1);
+                int t2 = dp(K, N-m);
+                
                 if (t1 < t2) {
-                    lo = x;
+                    lo = m;
                 } else if (t1 > t2) {
-                    hi = x;
+                    hi = m;
                 } else {
-                    lo = hi = x;
+                    lo = hi = m;
                 }
             }
 
-            ans = 1 + Math.min(Math.max(dp(K-1,lo-1), dp(K, N-lo)),
-                                Math.max(dp(K-1, hi-1), dp(K, N-hi)));
+            ans = 1 + Math.min(Math.max(dp(K-1, lo-1), dp(K, N-lo)), 
+                            Math.max(dp(K-1, hi-1), dp(K, N-hi)));
         }
 
         memo.put(key, ans);
-
         return ans;
     }
+
+    
+
+    
 }
