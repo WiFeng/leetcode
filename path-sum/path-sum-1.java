12bad4ca54ca19c02f1764b0d9c70c3956049356
Kevon Liu <admin@521-wf.com>
2020-09-03 16:41:25 +0800

    add path-sum-1.java

---
+++ path-sum/path-sum-1.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+
+        if (root.left == null && root.right == null) {
+            if (sum == root.val) {
+                return true;
+            }
+            return false;
+        }
+
+        if (root.left != null) {
+            if (hasPathSum(root.left, sum - root.val)) {
+                return true;
+            }
+        }
+
+        if (root.right != null) {
+            if (hasPathSum(root.right, sum - root.val)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+}
